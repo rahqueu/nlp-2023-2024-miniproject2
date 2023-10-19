daddff08592ebe49c3c3ba1b258ed848406be763
rahqueu <raquel.cardoso@gmail.com>
2023-10-19 22:18:24 +0100

    changes

--- evaluation_logreg.py
+++ evaluation_logreg.py
@@ -5,7 +5,6 @@ import re
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
-from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
@@ -13,13 +12,18 @@ from sklearn.metrics import classification_report
 from sklearn.metrics import accuracy_score
 
 train_path = './train.txt'
-
-df = pd.read_csv(train_path, sep = '\t', names = ['label', 'review'])
-print(df.head())
+train_df = pd.read_csv(train_path, sep='\t', names=['label', 'review'])
+print(train_df.head())
 print("\n")
 
-X = df['review']
-y = df['label']
+test_path = './test_just_reviews.txt'
+test_df = pd.read_csv(test_path, sep='\t', header=None, names=['review'])
+print(test_df.head())
+print("\n")
+
+X_train = train_df['review']
+y_train = train_df['label']
+X_test = test_df['review']
 
 def apply_preprocessing(text):
     contractions = {
@@ -77,7 +81,7 @@ def apply_preprocessing(text):
         if(all(char in string.punctuation for char in token)):
             tokens.remove(token)
 
-    stop_words = set(stopwords.words('english'))
+    stop_words = stopwords.words('english')
     filtered_tokens = [word for word in tokens if word not in stop_words]
 
     lemmatizer = WordNetLemmatizer()
@@ -85,63 +89,23 @@ def apply_preprocessing(text):
 
     return ' '.join(lemmatized_tokens)
 
+for i in range(len(X_train)):
+    X_train[i] = apply_preprocessing(X_train[i])
+    
+for i in range(len(X_test)):
+    X_test[i] = apply_preprocessing(X_test[i])
 
-for i in range(len(X)):
-    X[i] = apply_preprocessing(X[i])
-
-# missing pre-processing step: tokenizaition, lowercasing, tirar stop words, pontuação e outros caracteres random
-
-# dividing data according to train.txt (1400 lines) and test_just_reviews.txt (200 lines) sizing -> 0.125 test size
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.125, random_state = 1) 
-print("Training data shape: ", X_train.shape)
-print("Testing data shape: ", X_test.shape)
-print("\n")
-
-# feature extraction: tf-idf bc its more complex than bag of words and fixes some issues w it but we can also try it
-tfidf_vectorizer = TfidfVectorizer(max_features=5000)
+tfidf_vectorizer = TfidfVectorizer(max_features = 5000)
 X_train_tfidf = tfidf_vectorizer.fit_transform(X_train)
 
-# model : SVC
-def SVC_TFIDF():
-    svm_classifier = SVC(kernel = 'linear', C = 1.0)
-    svm_classifier.fit(X_train_tfidf, y_train)
+logistic_classifier = LogisticRegression(C=1.0, penalty='l2', solver='liblinear')
+logistic_classifier.fit(X_train_tfidf, y_train)
 
-    X_test_tfidf = tfidf_vectorizer.transform(X_test)
-    y_test_pred = svm_classifier.predict(X_test_tfidf)
+X_test_tfidf = tfidf_vectorizer.transform(X_test)
+y_test_pred = logistic_classifier.predict(X_test_tfidf)
 
-    #evaluation
-    print("Testing Set Classification Report for Testing Set:")
-    print(classification_report(y_test, y_test_pred))
-    print("\n")
-    
-    testing_accuracy = accuracy_score(y_test, y_test_pred)
-    print("Testing Accuracy: " + str(testing_accuracy))
+output_path = 'test_just_labels_logreg.txt'
 
-'''
-# model : Logistic Regression
-def LR_TFIDF():
-    logistic_classifier = LogisticRegression(C = 1.0, penalty= 'l2', solver = 'lbfgs', max_iter = 1000)
-    logistic_classifier.fit(X_train_tfidf, y_train)
-    
-    X_test_tfidf = tfidf_vectorizer.transform(X_test)
-    y_test_pred = logistic_classifier.predict(X_test_tfidf)
-    
-    #evaluation
-    print("Testing Set Classification Report for Testing Set:")
-    print(classification_report(y_test, y_test_pred))
-    print("\n")
-    
-    testing_accuracy = accuracy_score(y_test, y_test_pred)
-    print("Testing Accuracy: " + str(testing_accuracy))
-'''
-
-print("SVC_TFIDF:\n")
-SVC_TFIDF()
-
-'''
-print("LR_TFIDF:\n")
-LR_TFIDF()
-'''
-
-# TODO:
-# meter os nomes bem
+with open(output_path, 'w') as output:
+    for label in y_test_pred:
+        output.write(label + '\n')
