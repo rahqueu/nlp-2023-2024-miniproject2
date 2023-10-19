6e820992bfcf43d439d8cec6cddfff3627270d72
Guilherme Patrão <guilherme.almeida.28@tecnico.ulisboa.pt>
2023-10-19 19:58:09 +0100

    Start preprocessing

--- testing.py
+++ testing.py
@@ -1,5 +1,10 @@
 import pandas as pd
 import sklearn
+import string
+import re
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVC
@@ -16,7 +21,75 @@ print("\n")
 X = df['review']
 y = df['label']
 
-# TODO:
+def apply_preprocessing(text):
+    contractions = {
+        "ain't": "am not",
+        "aren't": "are not",
+        "can't": "cannot",
+        "could've": "could have",
+        "couldn't": "could not",
+        "didn't": "did not",
+        "doesn't": "does not",
+        "don't": "do not",
+        "hadn't": "had not",
+        "hasn't": "has not",
+        "haven't": "have not",
+        "he's": "he is",
+        "i'd": "i would",
+        "i'll": "i will",
+        "i'm": "i am",
+        "i've": "i have",
+        "isn't": "is not",
+        "it's": "it is",
+        "let's": "let us",
+        "mustn't": "must not",
+        "shan't": "shall not",
+        "she's": "she is",
+        "should've": "should have",
+        "shouldn't": "should not",
+        "that's": "that is",
+        "there's": "there is",
+        "they're": "they are",
+        "wasn't": "was not",
+        "we'd": "we would",
+        "we're": "we are",
+        "weren't": "were not",
+        "what's": "what is",
+        "who's": "who is",
+        "won't": "will not",
+        "wouldn't": "would not",
+        "would've" : "would have",
+        "you'd": "you would",
+        "you're": "you are",
+    }
+
+    lowered = text.lower()
+
+    words = lowered.split()
+    result = []
+    for word in words:
+        result.append(contractions.get(word, word))
+    preproc = ' '.join(result)
+
+    tokens = word_tokenize(preproc, "english")
+
+    for token in tokens:
+        if(all(char in string.punctuation for char in token)):
+            tokens.remove(token)
+
+    stop_words = set(stopwords.words('english'))
+    filtered_tokens = [word for word in tokens if word not in stop_words]
+
+    lemmatizer = WordNetLemmatizer()
+    lemmatized_tokens = [lemmatizer.lemmatize(token) for token in filtered_tokens]
+
+    return ' '.join(lemmatized_tokens)
+
+
+for el in X:
+    el = apply_preprocessing(el)
+    print(el)
+
 # missing pre-processing step: tokenizaition, lowercasing, tirar stop words, pontuação e outros caracteres random
 
 # dividing data according to train.txt (1400 lines) and test_just_reviews.txt (200 lines) sizing -> 0.125 test size
